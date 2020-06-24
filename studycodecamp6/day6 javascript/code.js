7c26446c6155d9f481be36a72e39e75b4dff3dc6
chatwaroon244733 <chatwaroon_b@kkumail.com>
2020-06-24 11:09:44 +0700

    newcommit

---
+++ studycodecamp6/day6 javascript/code.js
@@ -0,0 +1,68 @@
+console.log("say hello");
+console.log("hello Chatwaroon ");
+console.log("mint ");
+console.log("say hello 111");
+
+var message = "สวัสดีค่ะ";
+console.log(message);
+
+var message1 = "สวัสดี";
+message1 = "ต้นมิ้นท์";
+console.log(message1); // อย่าลืมใส่เพื่อแสดงผล//
+
+//  var a = "a";
+//  var b = "b";
+//  var c = "c";
+
+//  var d ="d";  //ประกาศทีเดียวแล้วสามารถใช้ตัวแปลได้//
+    //  e = "e";
+
+// var h = "h", g ="g",k ="k";
+
+ 
+// var a
+// var b 
+// var c 
+
+//  var d 
+//      e 
+//      f
+
+// var g,h,t 
+// แล้วค่อยกำหนดค่าที่หลัง//
+
+
+// let number = 15
+// console.log(typeof 15)
+// console.log(typeof(15))
+// console.log(typeof number)
+// console.log(typeof(number))
+
+// let boolean1 = 2;
+// console.log(typeof boolean1);
+// boolean1=String(boolean1);
+// console.log(boolean1);
+// console.log(typeof boolean1)
+
+// let value = "9"/"4.5";
+// console.log(typeof value);
+//  console.log(value);
+
+//  let value = "9"/"2";
+//  let value1 = "9" - "2";
+//  let value2 = "9" * "2";
+//  let value3 = "9" + "2";
+// console.log(typeof value) // /
+// console.log(typeof value1) // -
+// console.log(typeof value2)// *
+// console.log(typeof value3) //+
+
+let value = "9"/"2";
+ let value1 = "9" - "2";
+ let value2 = "9" * "2";
+ let value3 = "9" + "2";
+console.log( value) // /
+console.log( value1) // -
+console.log( value2)// *
+console.log( value3) //+
+
